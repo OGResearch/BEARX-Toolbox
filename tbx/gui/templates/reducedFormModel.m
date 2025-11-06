fb2b739cb072dfb173d9f03f51da13508e3c6a5d
Jaromír Beneš <jaromir.benes@gmail.com>
2025-11-06 19:01:29 +0100

    Reopened #1, fixed #1

--- tbx/gui/templates/reducedFormModel.m
+++ tbx/gui/templates/reducedFormModel.m
@@ -10,7 +10,7 @@ meta = Meta( ...
 %% Prepare input data holder 
 
 % Load the input data table
-inputTbl = tablex.fromCsv(?INPUT_DATA_PATH?);
+inputTbl = tablex.fromFile(?INPUT_DATA_PATH?);
 printTable(inputTbl);
 
 % Create a data holder object

5122d08f1705bff19cb230314f75175896c7aee5
Jaromír Beneš <jaromir.benes@gmail.com>
2025-11-06 17:57:36 +0100

    Fixed #1

--- tbx/bearing/+tablex/fromFile.m
+++ tbx/bearing/+tablex/fromFile.m
@@ -16,7 +16,7 @@ function [tt, freq, tbl] = fromFile(fileName, options)
         options.ReplaceMissing (1, 1) logical = true
     end
 
-    options.FileType = tryAutoDetectFileType__(options.FileType, fileName);
+    [tbl, timeColumn] = readPlainTable__(fileName, options);
 
     periodConstructorDispatcher = struct( ...
         lower("sdmx"), @datex.fromSdmx ...
@@ -24,6 +24,32 @@ function [tt, freq, tbl] = fromFile(fileName, options)
     );
 
     periodConstructor = periodConstructorDispatcher.(lower(options.DateFormat));
+
+    if ~isempty(options.ConvertTo)
+        tbl = tablex.convert(tbl, options.ConvertTo, timeColumn=timeColumn);
+    end
+
+    if options.ReplaceMissing
+        tbl = tablex.replaceMissing(tbl, timeColumn=timeColumn);
+    end
+
+    %
+    % Convert the plain table to a timetable
+    %
+    [tt, freq] = tablex.fromTable( ...
+        tbl ...
+        , timeColumn=timeColumn ...
+        , frequency=options.Frequency ...
+        , periodConstructor=periodConstructor ...
+        , trim=options.Trim ...
+    );
+
+end%
+
+
+function [tbl, timeColumn] = readPlainTable__(fileName, options)
+    %[
+    options.FileType = tryAutoDetectFileType__(options.FileType, fileName);
 
     args = {
         "textType", "string" ...
@@ -37,30 +63,44 @@ function [tt, freq, tbl] = fromFile(fileName, options)
     end
 
     %
-    % Read the files as a plain table
+    % If a time column name is specified, try first to load the file as a
+    % table without row names, and check if the columns include a time
+    % column.
+    %
+    % If not, throw a warning, and try to read the file as a table with row
+    % names instead.
+    %
+    % If no time column name is specified, read the file as a table with
+    % row names, create an auto name for the time column ("Time"), and add
+    % the row names as a new first data column, and remove row names.
     %
 
-    tbl = readtable(fileName, args{:});
+    timeColumn = options.TimeColumn;
 
-    if ~isempty(options.ConvertTo)
-        tbl = tablex.convert(tbl, options.ConvertTo, timeColumn=options.TimeColumn);
+    if ~isequal(timeColumn, "")
+        tbl = readtable(fileName, args{:});
+        names = tablex.names(tbl);
+        if ismember(timeColumn, names)
+            return
+        end
+        warning(join([ ...
+            "The spreadsheet does not contain the time column '%s'.", ...
+            "Will read the file again assuming the time column has no name.", ...
+        ], newline()), timeColumn);
     end
-
-    if options.ReplaceMissing
-        tbl = tablex.replaceMissing(tbl, timeColumn=options.TimeColumn);
+    
+    tbl = readtable(fileName, "readRowNames", true, args{:});
+    timeData = reshape(string(tbl.Properties.RowNames), [], 1);
+    if isequal(timeColumn, "")
+        timeColumn = "Time";
     end
-
-    %
-    % Convert the plain table to a timetable
-    %
-    [tt, freq] = tablex.fromTable( ...
-        tbl ...
-        , timeColumn=options.TimeColumn ...
-        , frequency=options.Frequency ...
-        , periodConstructor=periodConstructor ...
-        , trim=options.Trim ...
+    tbl = addvars( ...
+        tbl, timeData ...
+        , newVariableNames=timeColumn ...
+        , before=1 ...
     );
-
+    tbl.Properties.RowNames = string.empty(0);
+    %]
 end%
 
 
@@ -84,4 +124,3 @@ function fileType = tryAutoDetectFileType__(fileType, fileName)
     end
     %]
 end%
-

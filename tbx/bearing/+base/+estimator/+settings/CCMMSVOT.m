3b4eca21c91181f66bedf3fc7d76b39c948b8474
kobercs13 <csaba.kober@ogresearch.com>
2025-11-12 09:44:36 +0100

    rename and cleanup

--- tbx/bearing/+base/+estimator/+settings/CCMMSVOT.m
+++ tbx/bearing/+base/+estimator/+settings/CCMMSVOT.m
@@ -4,14 +4,10 @@ classdef (CaseInsensitiveProperties=true) CCMMSVOT ...
 
     properties
         % Lower bound of the uniform prior on degrees of freedom for inverse gamma (Q diagonal);
-        QDoFLowerBound (1,1) double = 3 % lower bound of the uniform discrete prior distribution of 
-        % the degrees of freedom in the inverse gamma distribution of the diagobnal elements in matrix Q
-        % CCMM 3.2, originally Jacquier, Polson, and Rossi (2004)
+        DoFLowerBound (1,1) double = 3 % lower bound of degrees of freedom for the Student-t residuals
 
-        % Upper bound of the uniform prior on degrees of freedom for inverse gamma (Q diagonal);
-        QDoFUpperBound (1,1) double = 40 % upper bound of the uniform discrete prior distribution of 
-        % the degrees of freedom in the inverse gamma distribution of the diagonal elements in matrix Q
-        % CCMM 3.2, originally Jacquier, Polson, and Rossi (2004)
+         % Upper bound of the uniform prior on degrees of freedom for inverse gamma (Q diagonal);
+        DoFUpperBound (1,1) double = 40 % upper bound of degrees of freedom for the Student-t residuals
     end
 
 end
